ac11adade4f21a0982fd7a9b692488bd566662d4
untari <sastraembun0306@gmail.com>
2021-08-01 15:16:48 +0700

    remove firebase function

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const functions = require('firebase-functions');
 const express = require("express");
 const path = require('path');
 const bodyparser = require('body-parser');
@@ -33,5 +32,3 @@ app.get('/', (req,res) =>{
 })
 
 app.listen(port, () => {console.log("Listening to the server on http://localhost:3000")});
-
-exports.app = functions.https.onRequest(app);
